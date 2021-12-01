6c688c12b749b33ef1c90e212abeb617df65315c
Matthew Wilson <mfwilson1@gmail.com>
2021-11-30 22:00:41 -0800

    initial commit

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,25 @@
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) {
+    println("2021 Advent of Code day 1")
+
+    val input = ArrayList<Int>()
+    File("day1input").forEachLine { input.add(it.toInt()) }
+    println("There are ${input.size} measurements")
+
+    // Part 1
+    var timeMs = measureTimeMillis {
+        val part1Answer = input.mapIndexed { index, it -> if (index > 0) it > input[index-1] else false }.count { it }
+        println("$part1Answer measurements were larger than the previous one.")
+    }
+    println("Part 1 solution took $timeMs ms")
+
+    // Part 2
+    timeMs = measureTimeMillis {
+        val sums = input.mapIndexed { index, it -> if (index < input.size - 2) it + input[index+1] + input[index+2] else 0 }
+        val part2Answer = sums.mapIndexed { index, it -> if (index > 0) it > sums[index-1] else false }.count { it }
+        println("$part2Answer measurements were larger than the previous one.")
+    }
+    println("Part 2 solution took $timeMs ms")
+}
