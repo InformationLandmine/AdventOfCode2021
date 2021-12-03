f59ee442736d69d478a3af0241d87ff5af917acd
Matthew Wilson <mfwilson1@gmail.com>
2021-12-02 22:34:16 -0800

    fix my stupidity

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -5,21 +5,18 @@ fun main(args: Array<String>) {
 
     // Setup - Read the list of directions.
     val input = ArrayList<String>()
-    val numbits = 12
 
     File("day3input").forEachLine { input.add(it) }
     println("There are ${input.size} diagnostic readings")
 
     // Part 1 - find the most common bits and least common bits
-    var gammaStr = ""
-    var epsilonStr = ""
+    var gamma = ""
+    var epsilon = ""
     for (i in 0 until input[0].length - 1) {
-        gammaStr += if (input.count { it[i] == '0' } > input.count { it[i] == '1' }) '0' else '1'
-        epsilonStr += if (input.count { it[i] == '0' } < input.count { it[i] == '1' }) '0' else '1'
+        gamma += if (input.count { it[i] == '0' } > input.count { it[i] == '1' }) '0' else '1'
+        epsilon += if (input.count { it[i] == '0' } < input.count { it[i] == '1' }) '0' else '1'
     }
-    val gamma = Integer.parseInt(gammaStr, 2)
-    val epsilon = Integer.parseInt(epsilonStr, 2)
-    println("Part 1: gamma = $gamma; epsilon = $epsilon; gamma * epsilon = ${gamma * epsilon}")
+    println("Part 1: gamma * epsilon = ${gamma.toInt(2) * epsilon.toInt(2)}")
 
     // Part 2 - find the O2 generator and CO2 scrubber ratings
     var o2Generator = input.toList()
@@ -44,11 +41,5 @@ fun main(args: Array<String>) {
         }
         if (co2Scrubber.size <= 1) break@co2loop
     }
-
-    println(o2Generator[0])
-    println(co2Scrubber[0])
-
-    val o2rating = Integer.parseInt(o2Generator[0], 2)
-    val co2rating = Integer.parseInt(co2Scrubber[0], 2)
-    println("Part 2: o2rating = $o2rating; co2rating = $co2rating; o2rating * co2rating = ${o2rating * co2rating}")
+    println("Part 2: o2rating * co2rating = ${o2Generator[0].toInt(2) * co2Scrubber[0].toInt(2)}")
 }
