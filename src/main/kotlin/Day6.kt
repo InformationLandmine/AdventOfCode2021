50768f14f339078e8f62499ab20ed6ce45fada94
Matthew Wilson <mfwilson1@gmail.com>
2021-12-06 13:59:25 -0800

    simplify using deque

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -3,23 +3,14 @@ import java.io.File
 fun main(args: Array<String>) {
     println("2021 Advent of Code day 6")
 
-    // Setup - Read the fish "timers"
+    // Setup - Load the fish
     val fishInput = File("day6input").readLines()[0].split(",").map { it.toInt() }
     println("There are ${fishInput.size} fish")
     val testInput = listOf(3, 4, 3, 1, 2)
-    val input = testInput
+    val input = fishInput
 
-    val fish = arrayOf(
-        input.count { it == 0 }.toLong(),
-        input.count { it == 1 }.toLong(),
-        input.count { it == 2 }.toLong(),
-        input.count { it == 3 }.toLong(),
-        input.count { it == 4 }.toLong(),
-        input.count { it == 5 }.toLong(),
-        input.count { it == 6 }.toLong(),
-        input.count { it == 7 }.toLong(),
-        input.count { it == 8 }.toLong(),
-    )
+    val fish = ArrayDeque((0..8).map { 0L }.toList())
+    input.forEach { fish[it]++ }
 
     // Part 1 - Simulate 80 days.
     repeat(80) { iterateFish(fish) }
@@ -30,15 +21,8 @@ fun main(args: Array<String>) {
     println("After part 2 there are ${fish.sum()} fish")
 }
 
-fun iterateFish(fish: Array<Long>) {
-    val oldFishZero = fish[0]
-    fish[0] = fish[1]
-    fish[1] = fish[2]
-    fish[2] = fish[3]
-    fish[3] = fish[4]
-    fish[4] = fish[5]
-    fish[5] = fish[6]
-    fish[6] = fish[7] + oldFishZero
-    fish[7] = fish[8]
-    fish[8] = oldFishZero
+fun iterateFish(fish: ArrayDeque<Long>) {
+    val reproducingFish = fish.removeFirst()
+    fish.addLast(reproducingFish)
+    fish[6] += reproducingFish
 }
