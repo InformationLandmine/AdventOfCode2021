a107b1c3e1f24ab556331acc59d531a8143551b4
Matthew Wilson <mfwilson1@gmail.com>
2021-12-11 11:43:47 -0800

    improve day 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -3,44 +3,34 @@ import java.io.File
 fun main(args: Array<String>) {
     println("2021 Advent of Code day 2")
 
-    // Setup - Read the list of directions.
-    val DIRECTIONS_REGEX = """(\S+) (\d+)""".toRegex()
-    val directions = ArrayList<Pair<String, Long>>()
-    File("day2input").forEachLine {
-        val parse = DIRECTIONS_REGEX.matchEntire(it)
-        val direction = parse?.groups?.get(1)?.value?:"?"
-        val count = parse?.groups?.get(2)?.value?.toLong()?:0L
-        directions.add (Pair(direction, count))
-    }
-    println("There are ${directions.size} direction commands")
-
-    // Part 1 - follow the directions
-    var position = 0L
-    var depth = 0L
-
-    directions.forEach {
-        when (it.first) {
-            "forward" -> position += it.second
-            "up" -> depth -= it.second
-            "down" -> depth += it.second
+    // Setup - Read the list of commands.
+    val COMMANDS_REGEX = """(\S+) (\d+)""".toRegex()
+    val commands = File("day2input").readLines().map { with(COMMANDS_REGEX.matchEntire(it)!!) {
+            Pair(Command.valueOf(groups[1]?.value?:"?"), groups[2]?.value?.toLong()?:0L)
         }
     }
-    println("After following the directions, position = $position; depth = $depth; product = ${position * depth}")
+    println("There are ${commands.size} commands")
+
+    // Part 1 - follow the commands
+    val part1 = commands.fold(SubLocation()) { last, cmd ->
+        when (cmd.first) {
+            Command.forward -> last.copy(position = last.position + cmd.second)
+            Command.up -> last.copy(depth = last.depth - cmd.second)
+            Command.down -> last.copy(depth = last.depth + cmd.second)
+        }
+    }
+    println("Part 2 - the product of the submarine depth and position is ${part1.position * part1.depth}")
 
     // Part 2 - follow the new interpretation of the directions
-    position = 0L
-    depth = 0L
-    var aim = 0L
-
-    directions.forEach {
-        when (it.first) {
-            "forward" -> {
-                position += it.second
-                depth += aim * it.second
-            }
-            "up" -> aim -= it.second
-            "down" -> aim += it.second
+    val part2 = commands.fold(SubLocation()) { last, cmd ->
+        when (cmd.first) {
+            Command.forward -> last.copy(position = last.position + cmd.second, depth = last.depth + last.aim * cmd.second)
+            Command.up -> last.copy(aim = last.aim - cmd.second)
+            Command.down -> last.copy(aim = last.aim + cmd.second)
         }
     }
-    println("After following the new directions, position = $position; depth = $depth; product = ${position * depth}")
+    println("Part 2 - the product of the submarine depth and position is ${part2.position * part2.depth}")
 }
+
+enum class Command { forward, up, down }
+data class SubLocation(val position: Long = 0, val depth: Long = 0, val aim: Long = 0)
