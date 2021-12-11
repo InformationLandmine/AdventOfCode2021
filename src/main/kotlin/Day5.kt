4114599e384ed0d0d9c487e1647870e88670fbed
Matthew Wilson <mfwilson1@gmail.com>
2021-12-11 12:49:53 -0800

    improve day 5

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -5,52 +5,34 @@ fun main(args: Array<String>) {
 
     // Setup - Read the vent lines.
     val LINES_REGEX = """(\d+),(\d+) -> (\d+),(\d+)""".toRegex()
-    val ventLines = ArrayList<Pair<Point, Point>>()
-    File("day5input").forEachLine {
-        val parse = LINES_REGEX.matchEntire(it)
-        val p1 = Point(parse?.groups?.get(1)?.value?.toInt()!!, parse?.groups?.get(2)?.value?.toInt()!!)
-        val p2 = Point(parse?.groups?.get(3)?.value?.toInt()!!, parse?.groups?.get(4)?.value?.toInt()!!)
-        ventLines.add(Pair(p1, p2))
+    val ventLines = File("day5input").readLines().map { with(LINES_REGEX.matchEntire(it)!!) {
+            Line(Point(groups[1]?.value?.toInt()!!, groups[2]?.value?.toInt()!!),
+                Point(groups[3]?.value?.toInt()!!, groups[4]?.value?.toInt()!!))
+        }
     }
 
-    // Create the vent map.
-    val vents = VentMap()
-
-    // Part 1 - plot the vent map with just horizontal and vertical lines.
+    // Part 1 - Calculate how many of the horizontal and vertical vent lines overlap.
     val straightLines = ventLines.filter { (it.first.x == it.second.x) or (it.first.y == it.second.y) }
-    straightLines.forEach { vents.plotLine(it) }
-    println("The horizontal and vertical lines overlap at ${vents.overlapCount} points")
+    val part1 = straightLines.flatMap { it.getPoints() }.groupBy { it }.count { it.value.size > 1 }
+    println("The horizontal and vertical vent lines overlap at $part1 points")
 
-    // Part 2 - plot the rest of the lines.
-    val remainingLines = ventLines.filterNot { (it.first.x == it.second.x) or (it.first.y == it.second.y) }
-    remainingLines.forEach { vents.plotLine(it) }
-    println("After plotting the remaining lines, they overlap at ${vents.overlapCount} points")
-
-    // Part 3 - alternate method without using VentMap.
-    val overlaps = ventLines.flatMap { it.getPoints() }.groupBy { it }.count { it.value.size > 1 }
-    println("The new method found overlaps at $overlaps points")
+    // Part 2 - Calculate how many of the vent lines overlap.
+    val part2 = ventLines.flatMap { it.getPoints() }.groupBy { it }.count { it.value.size > 1 }
+    println("All the vent lines overlap at $part2 points")
 }
 
 data class Point(val x:Int, val y:Int)
+typealias Line = Pair<Point, Point>
 
-fun Pair<Point, Point>.getPoints(): List<Point> {
-    val dx = if (this.first.x < this.second.x) 1 else if (this.first.x > this.second.x) -1 else 0
-    val dy = if (this.first.y < this.second.y) 1 else if (this.first.y > this.second.y) -1 else 0
+fun Line.getPoints(): List<Point> {
+    val dx = if (first.x < second.x) 1 else if (first.x > second.x) -1 else 0
+    val dy = if (first.y < second.y) 1 else if (first.y > second.y) -1 else 0
     val result = ArrayList<Point>()
-    var newPoint = this.first
+    var newPoint = first
     result.add(newPoint)
-    while (newPoint != this.second) {
+    while (newPoint != second) {
         newPoint = Point(newPoint.x + dx, newPoint.y + dy)
         result.add(newPoint)
     }
     return result.toList()
-}
-
-class VentMap() {
-    val vents = HashMap<Point, Int>()
-    val overlapCount get() = vents.count { it.value > 1 }
-
-    fun plotLine(line: Pair<Point, Point>) {
-        line.getPoints().forEach { vents[it] = (vents[it] ?: 0) + 1 }
-    }
 }
