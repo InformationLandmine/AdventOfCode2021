f076e18051f1120805f3529a7ebf43bdc4bc29e3
Matthew Wilson <mfwilson1@gmail.com>
2021-12-12 14:03:49 -0800

    clean up

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -5,64 +5,46 @@ fun main(args: Array<String>) {
 
     // Setup - Load the cave connections.
     val LINES_REGEX = """(.*)-(.*)""".toRegex()
-    val paths = File("day12input").readLines().map { with(LINES_REGEX.matchEntire(it)!!) {
-            Pair(groups[1]?.value!!, groups[2]?.value!!)
-        }
-    }
+    val paths = File("day12input").readLines()
+        .map { with(LINES_REGEX.matchEntire(it)!!) { Pair(groups[1]?.value!!, groups[2]?.value!!) } }
 
     // Build the cave map.
-    val nodes = paths.flatMap { listOf(CaveNode(it.first, it.first.contains("[a-z]".toRegex())),
-        CaveNode(it.second, it.second.contains("[a-z]".toRegex()))) }.toSet()
+    val nodes = paths.flatMap { listOf(CaveNode(it.first), CaveNode(it.second)) }.toSet()
 
-    paths.forEach { cave ->
-        nodes.first { it.name == cave.first }.connections.add(nodes.first { it.name == cave.second })
-        nodes.first { it.name == cave.second }.connections.add(nodes.first { it.name == cave.first })
+    paths.forEach { path ->
+        nodes.first { it.name == path.first }.connections.add(nodes.first { it.name == path.second })
+        nodes.first { it.name == path.second }.connections.add(nodes.first { it.name == path.first })
     }
 
     // Part 1 - find all paths through the cave
-    val part1 = getRoutes(nodes.first { it.name == "start" }, ArrayList<CaveNode>())
+    val part1 = nodes.first { it.name == "start" }.getRoutes(ArrayList<CaveNode>())
     println("Part 1 - there are ${part1.size} routes through the cave")
 
     // Part 2
-    val part2 = getRoutes2(nodes.first { it.name == "start" }, ArrayList<CaveNode>())
+    val part2 = nodes.first { it.name == "start" }.getRoutes(ArrayList<CaveNode>(), 1)
     println("Part 2 - there are ${part2.size} routes through the cave")
 }
 
-fun getRoutes(node: CaveNode, currentRoute: MutableList<CaveNode>): ArrayList<List<CaveNode>> {
-    val routesToEnd = ArrayList<List<CaveNode>>()
-    if (node.small && currentRoute.contains(node))
-        return routesToEnd
-
-    currentRoute.add(node)
-    if (node.name == "end") {
-        routesToEnd.add(currentRoute)
-        return routesToEnd
-    }
-
-    node.connections.forEach { connection -> routesToEnd.addAll(getRoutes(connection, currentRoute.toMutableList())) }
-    return routesToEnd
-}
-
-fun getRoutes2(node: CaveNode, currentRoute: MutableList<CaveNode>): ArrayList<List<CaveNode>> {
-    val routesToEnd = ArrayList<List<CaveNode>>()
-
-    if (node.name == "start" && currentRoute.contains(node))
-        return routesToEnd
-
-    if (node.small && currentRoute.contains(node) &&
-        (currentRoute.filter { it.small }.count()) > currentRoute.filter { it.small }.distinct().count())
-        return routesToEnd
-
-    currentRoute.add(node)
-    if (node.name == "end") {
-        routesToEnd.add(currentRoute)
-        return routesToEnd
-    }
-
-    node.connections.forEach { connection -> routesToEnd.addAll(getRoutes2(connection, currentRoute.toMutableList())) }
-    return routesToEnd
-}
-
-data class CaveNode(val name:String, val small: Boolean) {
+class CaveNode(val name:String) {
+    val small = name.contains("[a-z]".toRegex())
     val connections = ArrayList<CaveNode>()
+
+    fun getRoutes(currentRoute: MutableList<CaveNode>, maxSmallRevisits: Int = 0): ArrayList<List<CaveNode>> {
+        val routesToEnd = ArrayList<List<CaveNode>>()
+
+        if (name == "start" && currentRoute.size > 0)
+            return routesToEnd
+
+        if (small && currentRoute.contains(this) &&
+            (currentRoute.count { it.small }) >= currentRoute.filter { it.small }.distinct().count() + maxSmallRevisits)
+            return routesToEnd
+
+        currentRoute.add(this)
+
+        if (name == "end")
+            return routesToEnd.apply { add(currentRoute) }
+
+        connections.forEach { routesToEnd.addAll(it.getRoutes(currentRoute.toMutableList(), maxSmallRevisits)) }
+        return routesToEnd
+    }
 }
