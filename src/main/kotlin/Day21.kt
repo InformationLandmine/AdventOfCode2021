6b7f4fff26a48a1466871675dc42b9ae5c83dbe4
Matthew Wilson <mfwilson1@gmail.com>
2021-12-21 15:33:56 -0800

    some clean-up

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,4 +1,3 @@
-import java.io.File
 
 fun main(args: Array<String>) {
     println("2021 Advent of Code day 21")
@@ -14,29 +13,18 @@ fun main(args: Array<String>) {
     game.p1Pos = p1Start
     game.p2Pos = p2Start
 
-    while (game.gameWinner == Player.none) {
-        game.play()
-    }
+    while (game.gameWinner == Player.none) { game.play() }
     val part1 = game.die.rollCount * if (game.gameWinner == Player.player1) game.p2Score else game.p1Score
     println("rollcount = ${game.die.rollCount}")
     println("gamewinner = ${game.gameWinner}")
     println("p1Score = ${game.p1Score}")
     println("p2Score = ${game.p2Score}")
-    println("Part 1 - $part1")
+    println("Part 1 answer - $part1")
 
     // Part 2 - play real Dirac Dice
-    val gameState = DiracDiceState(
-        p1Start,
-        p2Start,
-        0,
-        0,
-        TurnState.start,
-        0,
-        Player.player1
-    )
-    val winCounts = playDiracDice(gameState)
+    val winCounts = playDiracDice(DiracDiceState(p1Start, p2Start, 0, 0, TurnState.start, 0, Player.player1))
+    println("Part 2 - Player 1 wins in ${winCounts.first} universes; Player 2 wins in ${winCounts.second} ")
 
-    println("Part 2 ")
 }
 val scoreMap = HashMap<DiracDiceState, Pair<Long, Long>>()
 var p1wins = 0L
@@ -62,26 +50,19 @@ class DiracDiceBoard(val spaces: Int) {
     val die = DeterministicDie(100)
 
     fun moveP1(count: Int) {
-        repeat (count) { if (++p1Pos > spaces) p1Pos = 1 }
-        //position += dice_total % 10, and if position > 10, position -=10
-        //p1Pos += (count % spaces)
-        //if (p1Pos > count) p1Pos -= spaces
+        p1Pos = with((p1Pos + count) % spaces) { if (this == 0) spaces else this }
         p1Score += p1Pos
-        if (p1Score >= 1000)
-            gameWinner = Player.player1
+        if (p1Score >= 1000) gameWinner = Player.player1
     }
 
     fun moveP2(count: Int) {
-        repeat (count) { if (++p2Pos > spaces) p2Pos = 1 }
-//        p2Pos += (count % spaces)
-  //      if (p2Pos > count) p2Pos -= spaces
+        p2Pos = with((p2Pos + count) % spaces) { if (this == 0) spaces else this }
         p2Score += p2Pos
-        if (p2Score >= 1000)
-            gameWinner = Player.player2
+        if (p2Score >= 1000) gameWinner = Player.player2
     }
 
     fun play() {
-        val toMove = die.roll() + die.roll() + die.roll()
+        val toMove = (1..3).map { die.roll() }.sum()
         when (turn) {
             Player.player1 -> { moveP1(toMove); turn = Player.player2 }
             Player.player2 -> { moveP2(toMove); turn = Player.player1 }
@@ -109,54 +90,23 @@ fun playDiracDice(state: DiracDiceState): Pair<Long, Long> {
                 state.rollTotal = 0
                 state.turnState = TurnState.roll1
             }
-            TurnState.roll1 -> {
-                if (scoreMap.containsKey(state)) {
-                    return(scoreMap.getOrDefault(state, Pair(0L, 0L)))
-                } else {
-                    state.turnState = TurnState.roll2
-                    val result1 = playDiracDice(state.copy(rollTotal = state.rollTotal + 1))
-                    val result2 = playDiracDice(state.copy(rollTotal = state.rollTotal + 2))
-                    val result3 = playDiracDice(state.copy(rollTotal = state.rollTotal + 3))
-                    val winTotals = Pair(
-                        result1.first + result2.first + result3.first,
-                        result1.second + result2.second + result3.second
-                    )
-                    scoreMap[state.copy(turnState = TurnState.roll1)] = winTotals
-                    return winTotals
-                }
-            }
-            TurnState.roll2 -> {
-                if (scoreMap.containsKey(state)) {
-                    return(scoreMap.getOrDefault(state, Pair(0L, 0L)))
-                } else {
-                    state.turnState = TurnState.roll3
-                    val result1 = playDiracDice(state.copy(rollTotal = state.rollTotal + 1))
-                    val result2 = playDiracDice(state.copy(rollTotal = state.rollTotal + 2))
-                    val result3 = playDiracDice(state.copy(rollTotal = state.rollTotal + 3))
-                    val winTotals = Pair(
-                        result1.first + result2.first + result3.first,
-                        result1.second + result2.second + result3.second
-                    )
-                    scoreMap[state.copy(turnState = TurnState.roll2)] = winTotals
-                    return winTotals
-                }
-            }
+            TurnState.roll1,
+            TurnState.roll2,
             TurnState.roll3 -> {
                 if (scoreMap.containsKey(state)) {
                     return(scoreMap.getOrDefault(state, Pair(0L, 0L)))
                 } else {
-                    state.turnState = TurnState.move
-                    val result1 = playDiracDice(state.copy(rollTotal = state.rollTotal + 1))
-                    val result2 = playDiracDice(state.copy(rollTotal = state.rollTotal + 2))
-                    val result3 = playDiracDice(state.copy(rollTotal = state.rollTotal + 3))
+                    val nextState = TurnState.values()[state.turnState.ordinal + 1]
+                    val result1 = playDiracDice(state.copy(rollTotal = state.rollTotal + 1, turnState = nextState))
+                    val result2 = playDiracDice(state.copy(rollTotal = state.rollTotal + 2, turnState = nextState))
+                    val result3 = playDiracDice(state.copy(rollTotal = state.rollTotal + 3, turnState = nextState))
                     val winTotals = Pair(
                         result1.first + result2.first + result3.first,
                         result1.second + result2.second + result3.second
                     )
-                    scoreMap[state.copy(turnState = TurnState.roll3)] = winTotals
+                    scoreMap[state] = winTotals
                     return winTotals
                 }
-
             }
             TurnState.move -> {
                 when (state.turn) {
@@ -170,14 +120,14 @@ fun playDiracDice(state: DiracDiceState): Pair<Long, Long> {
                     Player.player1 -> {
                         state.p1Score += state.p1Pos
                         if (state.p1Score >= 21) {
-                            p1wins++;
+                            p1wins++
                             return Pair(1L, 0L)
                         }
                     }
                     Player.player2 -> {
                         state.p2Score += state.p2Pos
                         if (state.p2Score >= 21) {
-                            p2Wins++;
+                            p2Wins++
                             return Pair(0L, 1L)
                         }
                     }
